23983e35958ed10babba17d3d0be5fc2e0d05ca2
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2026-05-16 22:13:21 +0400

    next js cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      {" "}
-      <h1 className="text-4xl font-bold">Cueball</h1>{" "}
+      
+      <h1 className="text-4xl font-bold">Cueball</h1>
     </div>
   );
 }
